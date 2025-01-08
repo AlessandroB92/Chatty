7502face639c13edd1d72d760a692a3f1a8d0698
Alessandro Basile <alessandrobasile1992@gmail.com>
2025-01-08 23:24:54 +0100

    need to fix shared foto overflow

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -37,36 +37,6 @@ const Detail = () => {
               </div>
               <img src="./download.png" alt="" className="icon" />
             </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/26926276/pexels-photo-26926276/free-photo-of-scene-dell-amboseli.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/26926276/pexels-photo-26926276/free-photo-of-scene-dell-amboseli.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/26926276/pexels-photo-26926276/free-photo-of-scene-dell-amboseli.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon" />
-            </div>
           </div>
         </div>
         <div className="option">
