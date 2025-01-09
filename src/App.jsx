d686ce14bd28bd950c7f23ee229011acf4fcc535
Alessandro Basile <alessandrobasile1992@gmail.com>
2025-01-10 00:27:52 +0100

    add working chats

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,34 @@ import Notification from "./components/notification/Notification";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
 
-  useEffect(()=>{
-    const unSub = onAuthStateChanged(auth, (user)=>{
-      console.log(user);
-    })
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
 
-    return ()=>{
+    return () => {
       unSub();
     };
-  },[])
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
-          <Chat />
-          <Detail />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
         </>
       ) : (
         <Login />
